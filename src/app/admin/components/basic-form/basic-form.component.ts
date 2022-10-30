715f51dc9dbc7b964c9ad48ef9d79ae171c0b3cb
istrejo <istrejo2106@gmail.com>
2022-10-29 22:07:44 -0400

    FormGroup

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,31 +8,36 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-  nameField: FormControl = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(10),
-  ]);
-  emailField: FormControl = new FormControl('');
-  phoneField: FormControl = new FormControl('');
-  colorField: FormControl = new FormControl('#fff');
-  dateField: FormControl = new FormControl(0);
-  ageField: FormControl = new FormControl(15);
-
-  categoryField: FormControl = new FormControl('category-1');
-  tagField: FormControl = new FormControl('tag-1');
-
-  agreeField: FormControl = new FormControl(false);
-  genderField: FormControl = new FormControl();
-  zoneField: FormControl = new FormControl();
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#fe3232'),
+    date: new FormControl(''),
+    age: new FormControl(12),
+    category: new FormControl(''),
+    tag: new FormControl(''),
+    agree: new FormControl(''),
+    gender: new FormControl(''),
+    zone: new FormControl(''),
+  });
 
   constructor() {}
 
   ngOnInit(): void {
-    this.nameField.valueChanges.subscribe(console.log);
+    // this.nameField.valueChanges.subscribe(console.log);
   }
 
   getInputValue() {
     console.log(this.nameField.value);
+  }
+
+  save() {
+    console.log(this.form.value);
+  }
+
+  get nameField() {
+    return this.form.get('name');
   }
 
   get isNameFieldValid() {
@@ -41,5 +46,45 @@ export class BasicFormComponent implements OnInit {
 
   get isNameFieldInvalid() {
     return this.nameField.touched && this.nameField.invalid;
+  }
+
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get phoneField() {
+    return this.form.get('phone');
+  }
+
+  get colorField() {
+    return this.form.get('color');
+  }
+
+  get dateField() {
+    return this.form.get('date');
+  }
+
+  get ageField() {
+    return this.form.get('age');
+  }
+
+  get categoryField() {
+    return this.form.get('category');
+  }
+
+  get tagField() {
+    return this.form.get('tag');
+  }
+
+  get agreeField() {
+    return this.form.get('agree');
+  }
+
+  get genderField() {
+    return this.form.get('gender');
+  }
+
+  get zoneField() {
+    return this.form.get('zone');
   }
 }
